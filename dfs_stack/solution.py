0d00268e0c55cd3146eb668493549a4d8b0d7cb0
papichoolo <souravsahil100@gmail.com>
2023-11-21 13:02:57 +0530

    dfs with stack added

---
+++ dfs_stack/solution.py
@@ -0,0 +1,17 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if len(self.stack) < 1:
+            return None
+        return self.stack.pop()
+
+    def size(self):
+        return len(self.stack)
+
+    def is_empty(self):
+        return len(self.stack) == 0
