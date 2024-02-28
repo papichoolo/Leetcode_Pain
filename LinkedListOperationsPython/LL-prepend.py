c264c93d8cac292182bcd432f20ffa06605022da
papichoolo <souravsahil100@gmail.com>
2024-02-28 17:29:57 +0530

    insertatnode

--- LinkedListOperationsPython/LL-prepend.py
+++ LinkedListOperationsPython/LL-prepend.py
@@ -1,11 +1,13 @@
-class Node(self,value):
-    self.value = value
-    self.next = None
-
-class LinkedList(self):
-    self.head = None
-    self.tail = None
-    self.length = 0
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+class LinkedList:
+    def __init__(self,value):
+        new_node=Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
 
     def prepend(self,value):
         new_node = Node(value)
@@ -36,6 +38,16 @@ class LinkedList(self):
         for _ in range(index):
             temp = temp.next
         return temp
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
     
     def set_value(self,index,value):
         temp = self.get(index)
@@ -43,8 +55,30 @@ class LinkedList(self):
             temp.value = value
             return True
         return False
-
+    
+    def insert(self,index,value):
+        if index<0 and index>self.length:
+            return False
+        if index==0:
+            return self.prepend(value)
+        if index==self.length:
+            return self.append(value)
+        new_node=Node(value)
+        temp=self.get(index-1)
+        new_node.next=temp.next
+        temp.next=new_node
+        self.length+=1
+        return True
+    def print_list(self):
+        temp = self.head
+        while temp:
+            print(temp.value)
+            temp = temp.next
         #thank you stuffi
     #i hate living right about now, kill me
 
-    
+bruh= LinkedList(1)
+bruh.append(9)
+bruh.prepend(56)
+print(bruh.insert(1,"hello"))
+bruh.print_list()
---
+++ LinkedListOperationsPython/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+def print_list(self):
+        temp = self.head
+        while temp:
+            print(temp.value)
+            temp = temp.next
