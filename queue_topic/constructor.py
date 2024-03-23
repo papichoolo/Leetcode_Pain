f90774706c3ba3bf98d618d15a06f0b2c389fce6
papichoolo <souravsahil100@gmail.com>
2024-03-23 14:28:43 +0530

    enqueue operation

--- queue_topic/constructor.py
+++ queue_topic/constructor.py
@@ -15,7 +15,20 @@ class Queue:
         while temp is not None:
             print(temp.value)
             temp = temp.next
-
+    #enqueue operation
+    
+    def enqueue(self,data):
+        new_node=Node(data)
+        if self.first is None:
+            self.first=new_node
+            self.last=new_node
+        else:
+            self.last.next=new_node
+            self.last=new_node
+        self.length+=1
 
 q=Queue(4)
+q.printlist()
+q.enqueue(3)
+q.enqueue(78)
 q.printlist()
