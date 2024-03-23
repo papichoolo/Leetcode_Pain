7d959a5cba80aded20872783af40234b07305da4
papichoolo <souravsahil100@gmail.com>
2024-03-23 15:09:25 +0530

    Change in MyQueue class:
    - Removed a blank line
    - Changed the method `peek` to return the top element of the first stack (`self.stack1`) instead of the top element of the second stack (`self.stack2`).

--- queue_topic/constructor.py
+++ queue_topic/constructor.py
@@ -26,9 +26,29 @@ class Queue:
             self.last.next=new_node
             self.last=new_node
         self.length+=1
+    
+    #dequeing removes the first item of the list
+        # consider three scenarios: with no nodes, with  one node, and with more than one
+    
+    def dequeue(self):
+        if self.length==0:
+            return None
+        temp=self.first
+        if self.length==1:
+            self.first=None
+            self.last=None
+        else:
+            self.first=self.first.next
+            temp.next=None
+        self.length-=1
+        return temp.value
+            
 
 q=Queue(4)
 q.printlist()
 q.enqueue(3)
 q.enqueue(78)
+q.printlist()
+q.dequeue()
+print(q.dequeue())
 q.printlist()
