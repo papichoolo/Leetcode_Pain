7d41c25509d953c3284b5715a1d6d61b692d5e60
papichoolo <souravsahil100@gmail.com>
2024-05-16 18:51:07 +0530

    refactor: add method to add edges in Graph class
    This commit adds a new method, `add_edge`, to the `Graph` class in `constructor.py`. The `add_edge` method allows adding an edge between two vertices in the graph. It checks if both vertices exist in the graph and adds the edge bidirectionally. If the vertices are not found, it returns `False`.
    
    Co-authored-by: [Author Name]

--- graphs/constructor.py
+++ graphs/constructor.py
@@ -10,10 +10,17 @@ class Graph:
     def print_graph(self):
         for vertex in self.graph:
             print(vertex,":",self.graph[vertex])
+    def add_edge(self,v1,v3):
+        if v1 in self.graph.keys() and v3 in self.graph.keys():
+            self.graph[v1].append(v3)
+            self.graph[v3].append(v1)
+            return True
+        return False
 
 
 
 bruh = Graph()
 bruh.add_vert(1)
 bruh.add_vert(2)
+bruh.add_edge(2,1)
 bruh.print_graph()
