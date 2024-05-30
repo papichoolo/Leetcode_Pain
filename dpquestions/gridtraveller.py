38ce3f9c3e91b258f13dc490fd0e9699334b8172
papichoolo <souravsahil100@gmail.com>
2024-05-30 18:17:05 +0530

    gridtraveller

---
+++ dpquestions/gridtraveller.py
@@ -0,0 +1,12 @@
+def gridtraveller(m,n, memo={}):
+    key=str(m)+','+str(n)
+    if key in memo:
+        return memo[key]
+
+    
+    if m==1 and n==1: return 1
+    if m==0 or n==0: return 0
+    memo[key]= gridtraveller(m-1,n, memo) + gridtraveller(m,n-1, memo)
+    return memo[key]
+
+print(gridtraveller(18,18))
