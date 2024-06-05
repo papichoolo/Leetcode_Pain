65b041dbee210807058eeac014fb06221d9505a3
papichoolo <souravsahil100@gmail.com>
2024-06-05 19:13:57 +0530

    happy no

---
+++ happynumber/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        bruh= set()
+        #unhappy nos. will have LL cycle in other number than 1, 
+        while n not in bruh:
+            bruh.add(n)
+            n=self.addsq(n)
+            if n==1:
+                return True
+
+        return False
+    def addsq(self, n):
+        rem=0
+        ans=0
+        while(n!=0):
+            rem=n%10
+            ans+=rem**2
+            n=n//10
+        return ans
+
+    
