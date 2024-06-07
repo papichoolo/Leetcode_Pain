063c809891390131ff05ed5876c4db61397b4a38
papichoolo <souravsahil100@gmail.com>
2024-06-07 22:21:02 +0530

    added stuff

---
+++ hashtables/anagram_palindrome.py
@@ -0,0 +1,15 @@
+#This is an absolutely beatiful solution to the problem of check whether a permutation of a string can be a palindrome or not.
+
+def has_palindrome_permutation(the_string):
+    # Track characters we've seen an odd number of times
+    unpaired_characters = set()
+
+    for char in the_string:
+        if char in unpaired_characters:
+            unpaired_characters.remove(char)
+        else:
+            unpaired_characters.add(char)
+
+    # The string has a palindrome permutation if it
+    # has one or zero characters without a pair
+    return len(unpaired_characters) <= 1
