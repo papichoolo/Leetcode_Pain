6bab13a9f78ba272ce82fec6bbec327baed09818
papichoolo <souravsahil100@gmail.com>
2024-06-30 21:06:19 +0530

    buh

--- graphs/constructor.py
+++ graphs/constructor.py
@@ -1,6 +1,8 @@
 class Graph:
     def __init__(self):
         self.graph = {}
+    
+    
     def add_vert(self,value):
         if value not in self.graph.keys():
             self.graph[value]=[]
