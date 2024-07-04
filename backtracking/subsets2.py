f915acb0ad164199abdb0f4ce2bb837fb82ea4a5
papichoolo <souravsahil100@gmail.com>
2024-07-04 11:47:03 +0530

    ```text refactor: update subsets method in Solution class
    The subsets method in the Solution class has been updated to handle duplicate elements in the input array. This ensures that the subsets generated do not contain duplicate combinations.

--- backtracking/subsets2.py
+++ backtracking/subsets2.py
@@ -1,19 +1,30 @@
 class Solution:
-    def subsets(self, nums):
+    def subsets2(self,nums):
+        nums.sort()
         res=[]
+
         subset=[]
 
         def helper(i):
             if i>=len(nums):
                 res.append(subset.copy())
                 return
+            
+            #add the element to the subset
+
             subset.append(nums[i])
             helper(i+1)
+
             subset.pop()
+            #skipping the element and its duplicates
+
+            while i+1<len(nums) and nums[i]==nums[i+1]:
+                i+=1
             helper(i+1)
         helper(0)
+
         return res
     
-
 bruh=Solution()
-print(bruh.subsets([1,2,2]))
+print(bruh.subsets2([1,2,2]))
+             
