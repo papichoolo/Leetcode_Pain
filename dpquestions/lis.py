9297dc75ad1b714325cf47d6c7c760af71c8dc6f
papichoolo <souravsahil100@gmail.com>
2024-07-16 00:54:38 +0530

    minorstuf

--- dpquestions/lis.py
+++ dpquestions/lis.py
@@ -6,6 +6,6 @@ def lis(arr):
             if arr[i] > arr[j] and lis[i] < lis[j] + 1:
                 lis[i] = lis[j] + 1
     return max(lis)
-
+#Instead of returning the first element of DP, return the largest element
 #bottom up approach
 print(lis([4 ,2 ,8, 1, 18 ,14]))
